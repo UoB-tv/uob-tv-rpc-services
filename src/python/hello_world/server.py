167e3266ddbb7e64ffe4ee391f35b6a1f87bc6cc
Zaiyang Li <zl4917@my.bristol.ac.uk>
2018-12-01 13:59:21 +0000

    hello world, progress, need to fix import error

--- src/python/hello_world/server.py
+++ src/python/hello_world/server.py
@@ -1,21 +1,12 @@
-import grpc_service_generated.hello_pb2_grpc as hello_grpc
-import grpc_service_generated.hello_pb2 as hello
+from __future__ import absolute_import, print_function
+from hello_world.service.greeter import Greeter
 
-class Greeter(hello_grpc.GreeterServicer):
-    def SayHello(self, request, context):
-        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+from concurrent import futures
+import grpc
+import time
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    print("server listening on 50051")
-    try:
-        while True:
-            time.sleep(_ONE_DAY_IN_SECONDS)
-    except KeyboardInterrupt:
-        server.stop(0)
+    print("hello world")
 
 if __name__ == "__main__":
     serve()
