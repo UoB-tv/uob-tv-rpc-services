492b1ccdd7cc6d9ded823769b6489cff35a6540c
Zaiyang Li <zl4917@my.bristol.ac.uk>
2018-12-07 12:22:52 +0000

    added namespace to job template

--- src/python/video_transcode_job/video_transcoder/video_transcoder.py
+++ src/python/video_transcode_job/video_transcoder/video_transcoder.py
@@ -1,15 +1,6 @@
 from google.cloud import storage
+import os
 
-from google.auth import ServiceAccountCredentials
-
-
-credentials_dict = {
-    'type': 'service_account',
-    'client_id': os.environ['BACKUP_CLIENT_ID'],
-    'client_email': os.environ['BACKUP_CLIENT_EMAIL'],
-    'private_key_id': os.environ['BACKUP_PRIVATE_KEY_ID'],
-    'private_key': os.environ['BACKUP_PRIVATE_KEY'],
-}
 
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(
     credentials_dict
